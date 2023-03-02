6cd62557f1072bca9c870ff4c6dcd9388e8b7a10
nizaelgvalente <nizael.valente@arcotech.io>
2023-03-02 00:33:41 -0300

    install prisma

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import express from 'express'
 import cors from 'cors'
-
+import 'dotenv/config'
 const app = express()
 app.use(express.json())
 app.use(cors())
 const PORT = process.env.PROT || 3006
-app.listen(PORT,()=> console.log('Servidor rodadndo'))
+app.listen(PORT, () => console.log('Servidor rodadndo'))
